c376dbbad4cf87a5ccacbf9ec030dc663983c87e
knishihama <47240829+knishihama@users.noreply.github.com>
2019-02-28 00:08:29 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-// Nuxt と Builder モジュールを require します
-const { Nuxt, Builder } = require('nuxt')
-// Nuxt の設定ファイルを require します
-const config = require('./nuxt.config.js')
-// 新たに Nuxt のインスタンスを生成します
-const nuxt = new Nuxt(config)
-// 開発環境の場合にライブビルドとライブリロードを有効化します
-if (nuxt.options.dev) {
-  new Builder(nuxt).build()
-}
+const {Nuxt} = require('nuxt');
+const https = require('https');
+const isProd = (process.env.NODE_ENV === 'production');
+const port = process.env.PORT || 3000;
+const config = require('./nuxt.config.js');
+config.dev = !isProd;
+const nuxt = new Nuxt(config);
+const options = {};
+https.createServer(options, nuxt.render).listen(port);
+console.log('Server listening on `localhost:' + port + '`.');
