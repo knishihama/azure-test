cdcaad5212b836ea421ccbaf73970d12ccabd3b3
knishihama <47240829+knishihama@users.noreply.github.com>
2019-02-28 00:38:58 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const {Nuxt} = require('nuxt');
 const http = require('http');
-const host = process.env.HOST || 127.0.0.1;
-const port = process.env.PORT || 3000;
+const host = process.env.HOST || '127.0.0.1';
+const port = process.env.PORT || '3000';
 const config = require('./nuxt.config.js');
 const nuxt = new Nuxt(config);
 https.createServer(nuxt.render).listen(port, host);
