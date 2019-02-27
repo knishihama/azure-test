132a19da71ef5b1b039d1fdca3072dea2460d343
knishihama <47240829+knishihama@users.noreply.github.com>
2019-02-28 00:20:47 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const {Nuxt} = require('nuxt');
 const https = require('https');
-const isProd = (process.env.NODE_ENV === 'production');
 const port = process.env.PORT || 3000;
 const config = require('./nuxt.config.js');
-config.dev = !isProd;
 const nuxt = new Nuxt(config);
-const options = {};
-https.createServer(options, nuxt.render).listen(port);
+https.createServer(nuxt.render).listen(port);
 console.log('Server listening on `localhost:' + port + '`.');
